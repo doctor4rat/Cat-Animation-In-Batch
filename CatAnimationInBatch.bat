b7dccf6bd9094973683e6032ba1949419e992601
doctor4rat <155171108+doctor4rat@users.noreply.github.com>
2024-11-29 17:36:08 +0200

    Add files via upload

---
+++ CatAnimationInBatch.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:animation
+cls
+echo.
+echo   /\___/\
+echo  ( o   o )
+echo  (  =^=  )
+echo  (        )
+echo  (         )
+echo  (          )))))))))))
+timeout /t 1 /nobreak >nul
+
+cls
+echo.
+echo   /\___/\
+echo  ( -   - )
+echo  (  =^=  )
+echo  (        )
+echo  (         )
+echo  (          )))))))))))
+timeout /t 1 /nobreak >nul
+
+goto animation
